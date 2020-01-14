3c232e57032ad866b43f4a9aa7b2ef46b47d9dd7
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2020-01-14 13:25:27 +0100

    Add project files.

---
+++ CA20200114/Jatekos.cs
@@ -0,0 +1,18 @@
+﻿namespace CA20200114
+{
+    class Jatekos
+    {
+        public string Nev { get; set; }
+        public int[] Tippek { get; set; }
+        public Jatekos(string sor)
+        {
+            var t = sor.Split(' ');
+
+            Nev = t[0];
+
+            Tippek = new int[t.Length - 1];
+            for (int i = 0; i < Tippek.Length; i++)
+                Tippek[i] = int.Parse(t[i + 1]);
+        }
+    }
+}
---
+++ CA20200114/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CA20200114
+{
+    class Program
+    {
+        static List<Jatekos> jatekosok;
+        static int forduloIndex;
+        static void Main()
+        {
+            F2();
+            F3();
+            F4();
+            F5();
+            Console.ReadKey();
+        }
+
+        private static void F2()
+        {
+            jatekosok = new List<Jatekos>();
+            var sr = new StreamReader(@"..\..\Forras\egyszamjatek1.txt");
+
+            while (!sr.EndOfStream)
+                jatekosok.Add(new Jatekos(sr.ReadLine()));
+            sr.Close();
+        }
+
+        private static void F3()
+        {
+            Console.Write("3. feladat: ");
+
+            Console.Write("Játékosok száma: ");
+            Console.WriteLine($"{jatekosok.Count} fő");
+        }
+
+        private static void F4()
+        {
+            Console.Write("4. feladat: ");
+
+            Console.Write("Kérem a forduló sorszámát: ");
+            forduloIndex = int.Parse(Console.ReadLine()) - 1;
+        }
+
+        private static void F5()
+        {
+            Console.Write("5. feladat: ");
+
+            int sum = 0;
+            foreach (var j in jatekosok)
+                sum += j.Tippek[forduloIndex];
+
+            Console.Write(
+                "Megadott fordulóban a tippek átlaga: {0:0.00}",
+                sum / (float)jatekosok.Count);
+        }
+    }
+}
